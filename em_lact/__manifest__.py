5a79f42323e7095ac2b5a42ee8c707e1e204ba46
alphatule <alex.martin.gil@elpuig.xeill.net>
2024-05-15 15:22:30 +0000

    En lista

--- em_lact/__manifest__.py
+++ em_lact/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "em_lact",
+    'name': "Gestion Lacteos",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -10,8 +10,8 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Gestion Empresas/Prod",
+    'website': "http://empresa.es/",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -24,7 +24,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
@@ -32,4 +32,5 @@
     'demo': [
         'demo/demo.xml',
     ],
+    'application': True
 }
