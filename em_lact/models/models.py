7d3e372b7775acf6575d46fc2635569035a90b01
alphatule <alex.martin.gil@elpuig.xeill.net>
2024-05-16 15:24:52 +0000

    aaa

--- em_lact/models/models.py
+++ em_lact/models/models.py
@@ -8,11 +8,40 @@ class em_lact(models.Model):
     _description = 'em_lact.em_lact'
 
     name = fields.Char()
-    value = fields.Integer()
-    value2 = fields.Float(compute="_value_pc", store=True)
-    description = fields.Text()
+    fecha_creacion = fields.Date()
+    # valoracion =
+    direccion = fields.Date()
+    dueno = fields.Char()
+    num_empleados = fields.Integer()
+    tipo_empresa = fields.Char(compute="_tipo_empresa", store=True)
+    activo = fields.Boolean(compute="_tipo_empresa", store=True)
 
-    @api.depends('value')
-    def _value_pc(self):
+    @api.depends('num_empleados')
+    def _tipo_empresa(self):
         for record in self:
-            record.value2 = float(record.value) / 100
+            if dueno != "":
+                record.activo = True
+            else:
+                record.activo = False
+
+            if record.num_empleados < 10:
+                record.tipo_empresa = "Micropyme"
+            elif record.num_empleados < 50:
+                record.tipo_empresa = "Pequeña"
+            elif record.num_empleados < 250:
+                record.tipo_empresa = "Mediana"
+            elif record.num_empleados < 1000:
+                record.tipo_empresa = "Grande"
+            elif record.num_empleados < 10000:
+                record.tipo_empresa = "Multinacional"
+                
+
+    # name = fields.Char()
+    # value = fields.Integer()
+    # value2 = fields.Float(compute="_value_pc", store=True)
+    # description = fields.Text()
+
+    # @api.depends('value')
+    # def _value_pc(self):
+    #     for record in self:
+    #         record.value2 = float(record.value) / 100
